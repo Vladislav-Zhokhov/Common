5e777a6730da1406a97cddaccdec3aef93cac5b5
Vladislav <Vladislav@DESKTOP-A4OFBSV>
2020-11-01 18:40:03 +0300

    долбаный рот этого сопровождения

---
+++ Barnaul/Windows/BlankZapretaAZSoprovozhdenie.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Barnaul.Windows
+{
+    /// <summary>
+    /// Это вкладка бланк яапрета АЗ у сопровождения
+    /// </summary>
+    public partial class BlankZapretaAZSoprovozhdenie : Window
+    {
+        public BlankZapretaAZSoprovozhdenie()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_DannyePoTipyZrk_Click(object sender, RoutedEventArgs e)
+        {
+            new DannyePoTipuZrkWindow().Show();
+        }
+
+        private void Button_Close_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
